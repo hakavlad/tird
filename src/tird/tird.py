38383a4b67e11fc2046db4a97b3c4b45bf4e6664
Alexey Avramov <hakavlad@gmail.com>
2025-08-27 03:07:36 +0900

    Bump version to 0.22.0

--- src/tird/tird.py
+++ src/tird/tird.py
@@ -4687,7 +4687,7 @@ def main() -> NoReturn:
 
 
 # Version of the application
-APP_VERSION: Final[str] = '0.21.1'
+APP_VERSION: Final[str] = '0.22.0'
 
 # Information string for the application
 APP_INFO: Final[str] = f"""tird v{APP_VERSION}
